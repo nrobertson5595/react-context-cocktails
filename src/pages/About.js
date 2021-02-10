cd628314b1e5e4a2001f63bcfedbc24fd92f519d
Nick <nrobertson5595@gmail.com>
2021-02-10 15:50:21 -0500

    upload commit

---
+++ src/pages/About.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        lorem Ullamco dolor deserunt cupidatat ut elit elit ullamco
+        reprehenderit mollit nisi amet qui. Mollit duis voluptate in sit aliquip
+        aliquip qui Lorem nisi fugiat. Laborum ullamco nulla proident proident
+        adipisicing qui duis id proident irure. Consectetur dolore reprehenderit
+        pariatur irure ea proident consectetur ullamco laboris consectetur esse.
+        Aliqua consequat aute quis eu deserunt dolor tempor. Sit esse pariatur
+        proident fugiat ut veniam laboris labore fugiat deserunt voluptate
+        eiusmod. Aliquip qui nulla sunt consectetur pariatur enim commodo.
+        Nostrud Lorem irure adipisicing enim do nulla sint irure. Lorem
+        exercitation eu veniam est eu Lorem culpa voluptate excepteur elit anim
+        ullamco tempor occaecat. Aliquip irure ad non culpa qui labore qui magna
+        amet. Tempor est nostrud officia qui eiusmod. Minim in irure velit
+        aliqua pariatur magna excepteur ullamco sint duis voluptate. Veniam
+        velit aliqua et aliqua qui cillum mollit minim qui. Sit pariatur nisi
+        proident cupidatat voluptate. Sunt nisi duis reprehenderit non proident
+        non culpa dolore cupidatat. In amet dolore sit aliquip duis duis. Minim
+        mollit eu nostrud irure enim labore. Ullamco proident velit commodo id
+        eiusmod duis nostrud fugiat consectetur nulla id. Fugiat consectetur
+        enim adipisicing pariatur occaecat irure Lorem eu. Fugiat irure
+        excepteur pariatur sit veniam excepteur. Culpa labore eiusmod ad tempor
+        deserunt elit eiusmod magna sint. Tempor non adipisicing pariatur amet
+        commodo officia elit aliqua sunt commodo reprehenderit irure tempor sit.
+        Velit sit non adipisicing in et. Culpa nisi esse consequat deserunt.
+        Laborum ad cillum cillum ad qui amet laborum proident ipsum.
+      </p>
+    </section>
+  );
+};
+
+export default About;
